eb377f7ec625dc530af1f59b5c7ee923432020b2
anang <suwasto.anang@gmail.com>
2025-02-08 09:58:11 +0700

    - add gradle.properties to .gitignore - setup maven publication

--- sharedUi/build.gradle.kts
+++ sharedUi/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -6,6 +7,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 kotlin {
@@ -62,4 +64,44 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+mavenPublishing {
+
+    coordinates(
+        groupId = "io.github.suwasto",
+        artifactId = "kmp-capturable-compose",
+        version = "0.1.0"
+    )
+
+    pom {
+        name.set("Kotlin Multiplatform Screenshot Library for Jetpack Compose")
+        description.set("A lightweight Kotlin Multiplatform library for capturing Jetpack Compose composables as image bitmaps. Supports Android and iOS. Easily take screenshots of your composables for testing, previews, or sharing.")
+        url.set("https://github.com/suwasto/KMP-Capturable-Compose")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("suwasto")
+                name.set("Anang Suwasto")
+                email.set("suwasto.anang@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/suwasto/KMP-Capturable-Compose.git")
+            developerConnection.set("scm:git:ssh://github.com:suwasto/KMP-Capturable-Compose.git")
+            url.set("https://github.com/suwasto/KMP-Capturable-Compose")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
